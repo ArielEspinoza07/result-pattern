447ff992d3b492d97e19391a7cf559f199f916f6
ArielEspinoza07 <arielespinoza46@gmail.com>
2025-06-04 11:50:14 -0600

    feat(library): Result Pattern add missing results

---
+++ src/UseProxy.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArielEspinoza07\ResultPattern;
+
+use ArielEspinoza07\ResultPattern\Contracts\CreateFromMessageContract;
+use ArielEspinoza07\ResultPattern\Enums\HttpResponseStatusCode;
+use ArielEspinoza07\ResultPattern\Traits\CreateFromMessage;
+
+final readonly class UseProxy extends Result implements CreateFromMessageContract
+{
+    use CreateFromMessage;
+
+    public static function from(string|null $message = null, array $data = []): Result
+    {
+        return self::create(
+            true,
+            $message ?? HttpResponseStatusCode::UseProxy->message(),
+            HttpResponseStatusCode::UseProxy->value,
+            $data
+        );
+    }
+}

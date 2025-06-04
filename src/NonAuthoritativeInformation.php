54279644fb275173134a273f742a3cbe9a7ae5ac
ArielEspinoza07 <arielespinoza46@gmail.com>
2025-06-04 11:47:46 -0600

    feat(library): Result Pattern add missing results

---
+++ src/NonAuthoritativeInformation.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArielEspinoza07\ResultPattern;
+
+use ArielEspinoza07\ResultPattern\Contracts\CreateFromMessageAndDataContract;
+use ArielEspinoza07\ResultPattern\Contracts\CreateFromMessageContract;
+use ArielEspinoza07\ResultPattern\Enums\HttpResponseStatusCode;
+use ArielEspinoza07\ResultPattern\Traits\CreateFromMessage;
+use ArielEspinoza07\ResultPattern\Traits\CreateFromMessageAndData;
+
+final readonly class NonAuthoritativeInformation extends Result implements CreateFromMessageAndDataContract, CreateFromMessageContract
+{
+    use CreateFromMessage;
+    use CreateFromMessageAndData;
+
+    public static function from(string|null $message = null, array $data = []): Result
+    {
+        return self::create(
+            true,
+            $message ?? HttpResponseStatusCode::NonAuthoritativeInformation->message(),
+            HttpResponseStatusCode::NonAuthoritativeInformation->value,
+            $data
+        );
+    }
+}

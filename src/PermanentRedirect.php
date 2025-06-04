495f546b86ac30f7611f1913358f88ccfba26d6a
ArielEspinoza07 <arielespinoza46@gmail.com>
2025-06-04 12:03:19 -0600

    feat(library): Result Pattern delete tests add imports

--- src/PermanentRedirect.php
+++ src/PermanentRedirect.php
@@ -4,13 +4,16 @@ declare(strict_types=1);
 
 namespace ArielEspinoza07\ResultPattern;
 
+use ArielEspinoza07\ResultPattern\Contracts\CreateFromMessageAndDataContract;
 use ArielEspinoza07\ResultPattern\Contracts\CreateFromMessageContract;
 use ArielEspinoza07\ResultPattern\Enums\HttpResponseStatusCode;
 use ArielEspinoza07\ResultPattern\Traits\CreateFromMessage;
+use ArielEspinoza07\ResultPattern\Traits\CreateFromMessageAndData;
 
-final readonly class PermanentRedirect extends Result implements CreateFromMessageContract
+final readonly class PermanentRedirect extends Result implements CreateFromMessageContract, CreateFromMessageAndDataContract
 {
     use CreateFromMessage;
+    use CreateFromMessageAndData;
 
     public static function from(string|null $message = null, array $data = []): Result
     {

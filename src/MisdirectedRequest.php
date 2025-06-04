37a89f612f0d5d8bfd1361752af1a4409bb408e0
ArielEspinoza07 <arielespinoza46@gmail.com>
2025-06-04 11:55:06 -0600

    feat(library): Result Pattern add missing results

---
+++ src/MisdirectedRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArielEspinoza07\ResultPattern;
+
+use ArielEspinoza07\ResultPattern\Contracts\CreateFromMessageContract;
+use ArielEspinoza07\ResultPattern\Enums\HttpResponseStatusCode;
+use ArielEspinoza07\ResultPattern\Traits\CreateFromMessage;
+
+final readonly class MisdirectedRequest extends Result implements CreateFromMessageContract
+{
+    use CreateFromMessage;
+
+    public static function from(string|null $message = null, array $data = []): Result
+    {
+        return self::create(
+            false,
+            $message ?? HttpResponseStatusCode::MisdirectedRequest->message(),
+            HttpResponseStatusCode::MisdirectedRequest->value,
+            $data
+        );
+    }
+}

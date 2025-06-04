c009129cd33902ddb807896c2bc068f08052fa37
ArielEspinoza07 <arielespinoza46@gmail.com>
2025-06-04 14:05:27 -0600

    feat(library): Result Pattern clean signature

--- src/Contracts/CreateFromMessageAndDataContract.php
+++ src/Contracts/CreateFromMessageAndDataContract.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace ArielEspinoza07\ResultPattern\Contracts;
 
+use ArielEspinoza07\ResultPattern\Enums\HttpResponseStatusCode;
 use ArielEspinoza07\ResultPattern\Result;
 
 interface CreateFromMessageAndDataContract
 {
-    public static function fromMessageAndData(string $message, array $data): Result;
+    public static function fromMessageAndData(
+        string $message,
+        HttpResponseStatusCode|int $status,
+        array $data,
+    ): Result;
 }
--- src/Contracts/CreateFromMessageContract.php
+++ src/Contracts/CreateFromMessageContract.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace ArielEspinoza07\ResultPattern\Contracts;
 
+use ArielEspinoza07\ResultPattern\Enums\HttpResponseStatusCode;
 use ArielEspinoza07\ResultPattern\Result;
 
 interface CreateFromMessageContract
 {
-    public static function fromMessage(string $message): Result;
+    public static function fromMessage(
+        string $message,
+        HttpResponseStatusCode|int $status,
+    ): Result;
 }

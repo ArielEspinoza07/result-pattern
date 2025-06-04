66ec70ef5035bcffb2d476e8ba835bfce80ad5e2
ArielEspinoza07 <arielespinoza46@gmail.com>
2025-06-04 14:05:42 -0600

    feat(library): Result Pattern update methods with changes

--- src/Traits/CreateFromMessage.php
+++ src/Traits/CreateFromMessage.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace ArielEspinoza07\ResultPattern\Traits;
 
+use ArielEspinoza07\ResultPattern\Enums\HttpResponseStatusCode;
 use ArielEspinoza07\ResultPattern\Result;
 
 trait CreateFromMessage
 {
-    public static function fromMessage(string $message): Result
-    {
-        return self::from(message: $message);
+    public static function fromMessage(
+        string $message,
+        HttpResponseStatusCode|int $status,
+    ): Result {
+
+        return self::from(
+            message: $message,
+            status: $status,
+        );
     }
 }
--- src/Traits/CreateFromMessageAndData.php
+++ src/Traits/CreateFromMessageAndData.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 
 namespace ArielEspinoza07\ResultPattern\Traits;
 
+use ArielEspinoza07\ResultPattern\Enums\HttpResponseStatusCode;
 use ArielEspinoza07\ResultPattern\Result;
 
 trait CreateFromMessageAndData
 {
-    public static function fromMessageAndData(string $message, array $data): Result
+    public static function fromMessageAndData(
+        string $message,
+        HttpResponseStatusCode|int $status,
+        array $data,
+    ): Result
     {
         return self::from(
             message: $message,
+            status: $status,
             data: $data,
         );
     }

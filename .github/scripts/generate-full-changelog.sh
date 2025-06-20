1ae524b47cedb9da81cf3dbf3541ec5315e49376
Ariel Espinoza <arielespinoza46@gmail.com>
2025-06-19 18:08:15 -0600

    refactor(action): modify script

--- .github/scripts/generate-full-changelog.sh
+++ .github/scripts/generate-full-changelog.sh
@@ -2,28 +2,28 @@
 
 set -e
 
-# Get all tags sorted by version (oldest to newest)
-TAGS=($(git tag --sort=creatordate))
-
-# Get today's date
-TODAY=$(date +%F)
+# Get all tags sorted by date (newest to oldest)
+TAGS=($(git tag --sort=-creatordate))
 
 # Initialize final changelog
 FINAL_CHANGELOG="# Changelog\n"
 
 for ((i = 0; i < ${#TAGS[@]}; i++)); do
   TAG=${TAGS[$i]}
-  NEXT_TAG=${TAGS[$((i + 1))]}
+  PREV_TAG=${TAGS[$((i + 1))]}
 
-  # Determine range: last tag to current
-  if [ "$i" -eq 0 ]; then
-    RANGE=$(git rev-list --max-parents=0 HEAD)..$TAG
+  # Determine git range
+  if [ -z "$PREV_TAG" ]; then
+    RANGE="$TAG"
   else
-    RANGE="${TAGS[$((i-1))]}..$TAG"
+    RANGE="$PREV_TAG..$TAG"
   fi
 
+  # Get date from tag (fallback to today)
+  TAG_DATE=$(git log -1 --format=%ad --date=short "$TAG" 2>/dev/null || date +%F)
+
   # Start section
-  CHANGELOG="## $TAG - $TODAY\n"
+  CHANGELOG="## $TAG - $TAG_DATE\n"
 
   # Reset sections
   ADDED=""; FIXED=""; CHANGED=""; DOCS=""; OTHER=""
@@ -42,15 +42,15 @@ for ((i = 0; i < ${#TAGS[@]}; i++)); do
   done < <(git log $RANGE --pretty=format:"%s" --no-merges)
 
   # Append sections
-  [ -n "$ADDED" ] && CHANGELOG+="\n### Added\n$ADDED"
-  [ -n "$FIXED" ] && CHANGELOG+="\n### Fixed\n$FIXED"
+  [ -n "$ADDED" ]   && CHANGELOG+="\n### Added\n$ADDED"
+  [ -n "$FIXED" ]   && CHANGELOG+="\n### Fixed\n$FIXED"
   [ -n "$CHANGED" ] && CHANGELOG+="\n### Changed\n$CHANGED"
-  [ -n "$DOCS" ] && CHANGELOG+="\n### Docs\n$DOCS"
-  [ -n "$OTHER" ] && CHANGELOG+="\n### Other\n$OTHER"
+  [ -n "$DOCS" ]    && CHANGELOG+="\n### Docs\n$DOCS"
+  [ -n "$OTHER" ]   && CHANGELOG+="\n### Other\n$OTHER"
   [ -z "$ADDED$FIXED$CHANGED$DOCS$OTHER" ] && CHANGELOG+="\n_No changes found._"
 
   FINAL_CHANGELOG+="$CHANGELOG\n\n"
 done
 
-# Write final changelog
+# Write to file
 echo -e "$FINAL_CHANGELOG" > CHANGELOG.md
